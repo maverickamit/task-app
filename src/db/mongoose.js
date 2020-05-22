77a9b578584a4bc37a99c9ad4ca4c82582ffcfc6
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-05-22 22:20:23 +0530

    added mongoose model for users and tasks

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-app-api", {
   useNewUrlParser: true,
@@ -6,19 +7,63 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-app-api", {
 });
 
 const Task = mongoose.model("Task", {
-  description: { type: String },
-  completed: { type: Boolean },
+  description: { type: String, required: true },
+  completed: {
+    type: Boolean,
+    validate(value) {
+      if (value != true) {
+        return false;
+      }
+    },
+  },
 });
 
-const newtask = new Task({
-  description: "Take a walk outside",
+const User = mongoose.model("User", {
+  name: { type: String, required: true, trim: true },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 8,
+  },
+});
+
+const newTask = new Task({
+  description: "Complete your diet",
   completed: true,
+  email: "mike@aol.com",
+  password: "password",
 });
 
-newtask
+newTask
   .save()
   .then((result) => {
-    console.log(newtask);
+    console.log(newTask);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const newUser = new User({
+  name: "Amit",
+  email: "mike@aol.com",
+  password: "password123",
+});
+
+newUser
+  .save()
+  .then((result) => {
+    console.log(newUser);
   })
   .catch((err) => {
     console.log(err);
