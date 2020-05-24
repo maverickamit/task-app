1d566462bd1c83957f3528a23fe3aef04efbabfd
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-05-24 18:55:35 +0530

    created tasks endpoint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 
 const port = process.env.PORT || 3001;
 require("./db/mongoose");
@@ -11,6 +12,7 @@ app.use(express.json());
 
 //Creating REST API endpoints
 
+//Creating Users endpoint
 app.post("/users", (req, res) => {
   const user = new User(req.body);
   user
@@ -22,6 +24,30 @@ app.post("/users", (req, res) => {
       res.status(400).send(err);
     });
 });
+
+//Creating Tasks endpoint
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+  task
+    .save()
+    .then((result) => {
+      res.send(task);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+//Hashing Password
+
+const hashing = async () => {
+  const Password = "1234522";
+  const hashedPassword = await bcrypt.hash(Password, 8);
+  console.log(Password, hashedPassword);
+  const isMatch = await bcrypt.compare("Password", hashedPassword);
+  console.log(isMatch);
+};
+hashing();
 
 //Serving the app on port 3000
 app.listen(port, () => {
