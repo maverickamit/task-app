7c4fc8672bf6b45a57a032cd77705727d1522c30
Amit <29959138+maverickamit@users.noreply.github.com>
2020-08-12 17:27:44 +0530

    deleting avatar field before sending user document to client

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-
 const port = process.env.PORT || 3001;
 require("./db/mongoose");
-const User = require("./models/user");
-const Task = require("./models/task");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 const app = express();
@@ -13,17 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-
-// JSON web token
-
-const myfunction = async () => {
-  const token = jwt.sign({ _id: "abc123" }, "newprojectforme", {
-    expiresIn: "7 days",
-  });
-  // console.log(token);
-  console.log(jwt.verify(token, "newprojectforme"));
-};
-myfunction();
 
 //Serving the app on port 3000
 app.listen(port, () => {
