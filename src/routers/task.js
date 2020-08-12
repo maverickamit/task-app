6ff8919566bffd39406b6ecf2318663eef040f17
Amit <29959138+maverickamit@users.noreply.github.com>
2020-08-12 12:17:29 +0530

    added auth to update tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -60,7 +60,7 @@ router.get("/tasks/:id", auth, async (req, res) => {
 
 //Updating Task details
 
-router.patch("/tasks/:id", async (req, res) => {
+router.patch("/tasks/:id", auth, async (req, res) => {
   const _id = req.params.id;
   const body = req.body;
   const allowedUpdates = ["description", "completed"];
@@ -74,11 +74,15 @@ router.patch("/tasks/:id", async (req, res) => {
     });
   }
   try {
-    const task = await Task.findByIdAndUpdate(_id);
+    const task = await Task.findOne({
+      _id,
+      owner: req.user._id,
+    });
 
     if (!task) {
       res.status(404).send();
     }
+
     updatesUsed.forEach((update) => {
       task[update] = body[update];
     });
