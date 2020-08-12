1d84f629c5d79d0b075e8675edaddabf71d2e4a3
Amit <29959138+maverickamit@users.noreply.github.com>
2020-08-12 17:04:08 +0530

    added error handling from middleware:update avatar route

--- src/routers/user.js
+++ src/routers/user.js
@@ -35,6 +35,7 @@ const upload = multer({
     cb(undefined, true);
   },
 });
+
 router.post(
   "/users/me/avatar",
   auth,
@@ -43,6 +44,9 @@ router.post(
     req.user.avatar = req.file.buffer;
     await req.user.save();
     res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
   }
 );
 
